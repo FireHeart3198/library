0552e4a3eed6291b5faf3f404e9466bb7df75af1
mb <115391907+FireHeart3198@users.noreply.github.com>
2024-03-12 18:24:40 +1100

    Add function to store new books to library array

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,3 +9,16 @@ function Book(title, author, pages, read) {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
     }
 }
+
+function addBookToLibrary() {
+    let title = prompt("Book Title", 'Cotillion')
+    let author = prompt("Book Author", 'Georgette Heyer')
+    let pages = prompt("Total number of pages?", '416')
+    let read = prompt("Read?", 'not yet read')
+    
+    const book = new Book(title, author, pages, read)
+    myLibrary.push(book)
+}
+
+addBookToLibrary();
+console.log(myLibrary)
