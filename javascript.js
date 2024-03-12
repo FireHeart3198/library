c23384eb616867b0eb911fed45c18bb62b6e184c
mb <115391907+FireHeart3198@users.noreply.github.com>
2024-03-12 19:59:53 +1100

    Add function to display books from Library array

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const body = document.querySelector('body')
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -10,15 +11,33 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary() {
-    let title = prompt("Book Title", 'Cotillion')
-    let author = prompt("Book Author", 'Georgette Heyer')
-    let pages = prompt("Total number of pages?", '416')
-    let read = prompt("Read?", 'not yet read')
-    
+function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read)
     myLibrary.push(book)
 }
 
-addBookToLibrary();
-console.log(myLibrary)
+function displayLibrary() {
+    myLibrary.forEach(book => {
+       const bookCard = document.createElement('div');
+       bookCard.classList.add('book-card');
+       for(info in book) {
+        if(typeof book[info] !== "function") {
+            const div = document.createElement('div')
+            const className = info;
+            div.classList.add(`${className}`)
+            div.textContent = book[info]
+            bookCard.append(div)
+        }
+       }
+       body.append(bookCard);
+    })
+}
+
+// books to check display
+addBookToLibrary('Cotillion', 'Georgette Heyer', '416', 'not yet read');
+addBookToLibrary('Princess Ben', 'Catherine Gilbert Murdock', '344', 'read');
+addBookToLibrary('Swift', 'R.J. Anderson', '356', 'not yet read');
+addBookToLibrary('Cinder', 'Marissa Meyer', '400', 'not yet read');
+addBookToLibrary('Six of Crows', 'Leigh Bardugo', '465', 'read');
+console.log(myLibrary);
+displayLibrary();
